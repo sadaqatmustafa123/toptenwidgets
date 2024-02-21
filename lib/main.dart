9c08043fb05408a03441dc2e3f89186c346818dc
sadaqatmustafa123 <sadaqatmustafa12345@gmail.com>
2024-02-22 00:25:22 +0500

    done some changes

--- lib/main.dart
+++ lib/main.dart
@@ -21,78 +21,8 @@ class MyApp extends StatelessWidget {
             ),
           ),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    child: Center(child: Text('Container 1')),
-                    height: 150,
-                    width: 80,
-                    color: Colors.red,
-                  ),
-                )
-                // Container(
-                //   height: 150,
-                //   width: 80,
-                //   color: Colors.red,
-                // ),
-                ,
-                Expanded(
-                  flex: 2,
-                  child: Container(
-                    child: Center(child: Text('Container 1')),
-                    height: 150,
-                    width: 80,
-                    color: Colors.green,
-                  ),
-                )
-              ],
-            ),
-            Center(
-              child: InkWell(
-                onTap: () => {
-                  print('Button pressed'),
-                },
-                child: Container(
-                  height: 200,
-                  width: 250,
-                  child: Center(
-                    child: Text(
-                      'Button',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                  decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 119, 221, 211),
-                      borderRadius: BorderRadius.circular(20),
-                      // border: Border.all(
-                      //   color: const Color.fromARGB(255, 72, 65, 5),
-                      //   width: 2,
-                      // ),
-                      boxShadow: [
-                        const BoxShadow(
-                          color: Colors.grey,
-                          blurRadius: 8,
-                          spreadRadius: 2,
-                        )
-                      ],
-                      image: DecorationImage(
-                        image: NetworkImage(
-                            'https://images.pexels.com/photos/2559941/pexels-photo-2559941.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
-                        fit: BoxFit.fitHeight,
-                      )),
-                ),
-              ),
-            )
-          ],
+        body: const Scaffold(
+          body: Text('Hello World'),
         ),
       ),
     );
